6ec85ea01e2fe6667ecca2f69c761371a5a9c52b
tmohlala <tmohlala@student.wethinkcode.co.za>
2018-02-20 15:11:53 +0200

    update

---
+++ ft_print_number.c
@@ -0,0 +1,8 @@
+void ft_putchar(char c);
+
+int main() {
+    char c = '0';
+    while(c++ <= '9') {
+        ft_putchar(c);
+    }
+}
--- ft_print_reverse_alphabet.c
+++ ft_print_reverse_alphabet.c
@@ -1,12 +1,10 @@
 #include <unistd.h>
 
-void ft_putchar(char c) {
-    write(1, &c, 1);
-}
+void ft_putchar(char c);
 
 int main() {
     char c = 'z';
-    
+
     while(c >= 'a') {
         ft_putchar(c);
         c--;
