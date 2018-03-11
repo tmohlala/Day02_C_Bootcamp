9ca412d182e6171fce37accb180e6067ea44b1ba
Kingh-T <mohlala.tumelo@gmail.com>
2018-03-11 14:16:29 +0200

    update

--- Day04/ft_find_next_prime.c
+++ Day04/ft_find_next_prime.c
@@ -17,3 +17,15 @@ int ft_is_prime(int nb) {
         return 1;
     }
 }
+
+int ft_find_next_prime(int nb) {
+    if(nb <= 2) {
+        return 2;
+    }
+    while(1) {
+        if(ft_is_prime(nb)) {
+            return nb;
+        }
+        nb++;
+    }
+}
