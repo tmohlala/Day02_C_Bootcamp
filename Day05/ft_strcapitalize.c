3b9def122165971f6c584541e25e9439ec7051d1
tmohlala <tmohlala@student.wethinkcode.co.za>
2018-03-12 07:01:09 +0200

    updated ft_strcapitalize

--- Day05/ft_strcapitalize.c
+++ Day05/ft_strcapitalize.c
@@ -5,9 +5,23 @@ char    ft_toUpper(char c) {
     return c;
 }
 
-int ft_isAlpha(char c) {
-    if(c >= 'a' && c <= 'z') {
+char    ft_toLower(char c) {
+    if(c >= 'A' && c <= 'Z') {
+        c += 32;
+    }
+    return c;
+}
+
+int ft_isalnum(char c) {
+    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
         return 1; 
+    }
+    return 0;
+}
+
+int ft_isUpper(char c) {
+    if(c >= 'A' && c <= 'Z') {
+        return 1;
     }
     return 0;
 }
@@ -16,16 +30,13 @@ char    *ft_strcapitalize(char *str) {
     int i = 0;
 
     while(str[i] != '\0') {
-        if(!ft_isAlpha(str[i - 1])) {
+        if(!ft_isalnum(str[i - 1])) {
             str[i] = ft_toUpper(str[i]);
+        }
+        if(ft_isalnum(str[i - 1]) && ft_isUpper(str[i])) {
+            str[i] = ft_toLower(str[i]);
         }
         i++;
     }
     return str;
-}
-
-#include <stdio.h>
-int main() {
-    char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-    printf("%s\n", ft_strcapitalize(str));
 }
